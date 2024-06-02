f4df58d5fc70dd77c0bd20f8a5a04a7a5627a204
Jairo <kidandcat@gmail.com>
2024-06-02 08:18:38 +0200

    simplify db init

--- backend/backend.go
+++
@@ -1,5 +0,0 @@
-package backend
-
-import "gorm.io/gorm"
-
-var db *gorm.DB
--- backend/db.go
+++ backend/db.go
@@ -1,13 +1,17 @@
-//go:build !wasm
-
 package backend
 
 import (
+	"github.com/maxence-charriere/go-app/v10/pkg/app"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func init() {
+	if app.IsClient {
+		return
+	}
 	var err error
 	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
